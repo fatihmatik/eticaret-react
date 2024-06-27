37db77b59418713273728d98f29d15befdec3bdb
Ahsen Arslantepe <ahsen.arslantepe@turk.net>
2024-06-27 18:45:01 +0300

    deneme

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -75,7 +75,7 @@ const LogIn = () => {
           </div>
           <div className="flex items-center justify-between">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
               onClick={handleSignIn}
             >
